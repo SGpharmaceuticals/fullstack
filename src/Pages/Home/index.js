5542a0a24a080220bb137d77c9e7f732b33b37a4
SGPharmaceuticals <Sgpharmaceuticalsneutra@gmail.com>
2025-01-08 17:27:23 +0530

    8 jan commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import HomeBanner from "../../Components/HomeBanner";
 import banner1 from "../../assets/images/banner1.jpg";
+import banner2 from "../../assets/images/banner2.jpg";
+
 import Button from "@mui/material/Button";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import Rating from "@mui/material/Rating";
 import ProductItem from "../../Components/ProductItem";
 
+import banner3 from "../../assets/images/banner3.jpg";
+import banner4 from "../../assets/images/banner4.jpg";
+import newsLetterImg from "../../assets/images/newsLetterImg.png";
+
+import { CiMail } from "react-icons/ci";
 const Home = () => {
   var productSliderOptions = {
     dots: true,
@@ -22,12 +28,19 @@ const Home = () => {
   return (
     <>
       <HomeBanner />
+
       <section className="homeProducts">
         <div className="container">
           <div className="row">
             <div className="col-md-3">
-              <div className="banner">
-                <img src={banner1} className="cursor w-100" />
+              <div className="sticky">
+                <div className="banner">
+                  <img src={banner1} className="cursor w-100" />
+                </div>
+
+                <div className="banner mt-4">
+                  <img src={banner2} className="cursor w-100" />
+                </div>
               </div>
             </div>
             <div className="col-md-9 productRow">
@@ -93,41 +106,51 @@ const Home = () => {
                 </Button>
               </div>
 
-              <div className="product_row w-100 mt-4">
-                <Swiper
-                  slidesPerView={4}
-                  spaceBetween={0}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  modules={[Navigation]}
-                  className="mySwiper"
-                >
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-                </Swiper>
+              <div className="product_row productRow2 w-100 mt-4 d-flex">
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
               </div>
+
+              <div className="bannerSection d-flex mt-4 mb-5">
+                <div className="banner ">
+                  <img src={banner3} className="cursor w-100" />
+                </div>
+                <div className="banner">
+                  <img src={banner4} className="cursor w-100" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="newsLetterSection mt-3 mb-3 d-flex align-items-center ">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <p className="text-white mb-1">
+                Rs.100 discount for your first order
+              </p>
+              <h3 className="text-white">Join our newsletter and get...</h3>
+              <p className="text-light">
+                Join our email subscription now to get updates on <br />
+                promotions and coupons.
+              </p>
+
+              <form>
+                <CiMail />
+                <input type="text" placeholder="Your email address" />
+                <Button>Subscribe</Button>
+              </form>
+            </div>
+            <div className="col-md-6">
+              <img src={newsLetterImg} />
             </div>
           </div>
         </div>
